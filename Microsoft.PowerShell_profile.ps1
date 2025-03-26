8ee745c206b00f69e4cf1a14acc1a22e51d2ad57
Jojo Lindell <johannes152@hotmail.com>
2025-03-26 21:04:33 +0100

    first commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+function vibes { git status }
+function slay { git add . }
+function rizz { param($msg = "Quick Update") git commit -m $msg }
+function yeet { git push origin $(git branch --show current) }
+function skrrt { git pull origin $(git branch --show-current) }
+function cdMauiT { Set-Location "C:\Users\Johan\source\repos\MauiTrading"}
+function cdMauiB { Set-Location "C:\Users\Johan\source\repos\MauiBackend"}
+function nvimGame { Set-Location "C:\Users\Johan\LuaProjekt\codeCampLua"; nvim . }
+function cdLua { Set-Location "C:\Users\Johan\LuaProjekt"}
