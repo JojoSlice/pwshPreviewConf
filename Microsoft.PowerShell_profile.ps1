4bb131cd017c31e308d572331bf7c7f7f1945e44
Jojo Lindell <johannes152@hotmail.com>
2025-03-27 18:42:58 +0100

    Quick Update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,7 +3,7 @@ function slay { git add . }
 function rizz { param($msg = "Quick Update") git commit -m $msg }
 function yeet { git push origin $(git branch --show current) }
 function skrrt { git pull origin $(git branch --show-current) }
-function cdMauiT { Set-Location "C:\Users\Johan\source\repos\MauiTrading"}
-function cdMauiB { Set-Location "C:\Users\Johan\source\repos\MauiBackend"}
+function cdnvim { Set-Location "C:\Users\Johan\appdata\local\nvim"}
+function cdweb { Set-Location "C:\Users\Johan\web"}
 function nvimGame { Set-Location "C:\Users\Johan\LuaProjekt\codeCampLua"; nvim . }
 function cdLua { Set-Location "C:\Users\Johan\LuaProjekt"}
