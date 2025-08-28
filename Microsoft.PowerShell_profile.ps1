95b7be5d0ac44e2f6e515dba20ba3691f942d4ac
Jojo Lindell <johannes152@hotmail.com>
2025-08-28 20:45:10 +0200

    Quick update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,30 @@
 function vibes { git status }
 function slay { git add . }
 function rizz { param($msg = "Quick Update") git commit -m $msg }
-function yeet { git push origin $(git branch --show current) }
+function yeet { git push origin $(git branch --show-current) }
 function skrrt { git pull origin $(git branch --show-current) }
+function drip {
+    param([string]$msg = "Quick update")
+    git add .
+    git commit -m $msg
+    git push origin $(git branch --show-current)
+}
+function switch {
+    param([string]$Branch)
+    if (-not $Branch) {
+        git branch
+        return
+    }
+    git checkout $Branch
+}
+function mkbranch {
+    param([string]$Branch)
+    if (-not $Branch) {
+        Write-Host "❌ Ange ett branchnamn."
+        return
+    }
+    git checkout -b $Branch
+}
 function cdnvim { Set-Location "C:\Users\Johan\appdata\local\nvim"}
 function cdweb { Set-Location "C:\Users\Johan\web"}
-function nvimGame { Set-Location "C:\Users\Johan\LuaProjekt\codeCampLua"; nvim . }
 function cdLua { Set-Location "C:\Users\Johan\LuaProjekt"}
